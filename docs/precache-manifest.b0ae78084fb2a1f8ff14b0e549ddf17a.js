67aa31aa8441af208289b86487602ccb397b5eb7
Jesus Marenco <jesusdario10@hotmail.com>
2020-06-24 12:40:43 -0500

    cambiando path base

---
+++ docs/precache-manifest.b0ae78084fb2a1f8ff14b0e549ddf17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea2e2379253a4d269180e02f33dc328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f08719a6c17d8512c77",
+    "url": "/static/css/main.7017cc65.chunk.css"
+  },
+  {
+    "revision": "4878dc87bf6dd747ebfe",
+    "url": "/static/js/2.6fa727a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fa727a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f08719a6c17d8512c77",
+    "url": "/static/js/main.81a1a389.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
